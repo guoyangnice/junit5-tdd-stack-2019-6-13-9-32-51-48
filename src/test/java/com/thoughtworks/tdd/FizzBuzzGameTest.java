ccb2ec2d6b740397bbb8630633a46c9d7f2c9e04
guoyangnice <yangjienice@126.com>
2019-08-23 16:42:39 +0800

    add special number test

--- src/test/java/com/thoughtworks/tdd/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzGameTest.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzGameTest {
 	@Test
 	public void should_return_1_when_FizzBuzz_given_1() {
-		int number = 0;
+		int number = 1;
 		//given
 		FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
 		//when
 		String word = fizzBuzzGame.getWords(number);
 		//then
-		//Assertions.assertEquals("1", word);
-//		assertThat(word, is("1"));
+		Assertions.assertEquals("1", word);
 	}
 	
+	@Test
 	public void should_return_fizzBuzz_when_3_given_number() {
 		int number = 3;
 		//given
@@ -25,4 +25,16 @@ public class FizzBuzzGameTest {
 		//then
 		Assertions.assertEquals("fizz", word);
 	}
+	
+	@Test
+	public void should_fizzBuzzAll_when_number_given_number() {
+		int number = 15;
+		//given
+		FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+		//when
+		String word = fizzBuzzGame.getWords(number);
+		//then
+		Assertions.assertEquals("fizzBuzz", word);
+	}
+	
 }
